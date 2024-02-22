0e26ad94f651523531d153c1a615460ddee8f29c
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-02-22 04:24:14 +0100

    show the options and answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,10 @@
-function Question({ question }) {
+import Options from "./Options";
+
+function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
