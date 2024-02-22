86776dd44e518f5b58f42f5947804423f7377aff
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-02-22 03:34:32 +0100

    finished start screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -25,6 +26,8 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action unknow");
   }
@@ -48,7 +51,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
---
+++ src/Question.js
@@ -0,0 +1,9 @@
+function Question() {
+  return (
+    <div>
+      <h1>quiz</h1>
+    </div>
+  );
+}
+
+export default Question;
