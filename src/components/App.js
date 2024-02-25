a0fba9e8d63675578f243873a39e3c6920d7db96
Pinyada Sengate <pinyada.sengate@gmail.com>
2024-02-25 01:40:25 +0100

    add restart quiz

--- src/components/App.js
+++ src/components/App.js
@@ -55,6 +55,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
     default:
       throw new Error("Action unknow");
   }
@@ -114,6 +116,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
